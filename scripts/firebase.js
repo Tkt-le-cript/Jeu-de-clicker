15e7d2de6425564c3842b1cc1e4d3746cd6b2eae
Tkt-le-cript <ikg123454@gmail.com>
2025-06-08 05:49:24 +0200

    Create firebase.js

---
+++ scripts/firebase.js
@@ -0,0 +1,24 @@
+// Import depuis les CDN Firebase (pas besoin de npm)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+// ✅ Ta configuration Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBlA51alOyLgGOqhFLKorwuDYKBZ-QDw48",
+  authDomain: "cliquer-396f3.firebaseapp.com",
+  projectId: "cliquer-396f3",
+  storageBucket: "cliquer-396f3.firebasestorage.app",
+  messagingSenderId: "369745613359",
+  appId: "1:369745613359:web:c30370ac79583b29a6acda",
+  measurementId: "G-45BM05HW17"
+};
+
+// Initialisation Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+// Fonction exportée pour se connecter avec Google
+export function signInWithGoogle() {
+  return signInWithPopup(auth, provider);
+}
