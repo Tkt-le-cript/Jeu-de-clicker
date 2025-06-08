dc8911e662af1dc0f495971e6bba2cfc1ea8e096
Tkt-le-cript <ikg123454@gmail.com>
2025-06-08 05:26:37 +0200

    Update auth.js

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,1 +1,11 @@
+// scripts/auth.js
 
+document.getElementById('google-login')?.addEventListener('click', () => {
+  // Simulé pour l'instant
+  alert("Connexion Google non implémentée (mock)");
+  // window.location.href = "game.html?user=tonPseudoGoogle";
+});
+
+document.getElementById('guest-login')?.addEventListener('click', () => {
+  window.location.href = "guest-warning.html";
+});
